4e5951333f8ef421601c8d36a9cafee2de0fed63
Illarion Kovalchuk <illarion.kovalchuk@gmail.com>
2014-10-30 12:33:13 +0200

    start working on better throttling

--- py/radiosource/throttle.py
+++ py/radiosource/throttle.py
@@ -10,13 +10,25 @@ class Throttler(object):
 
         self.speed = (bitrate*1024) / 8
         self.input = input
+
         self.cnt = 0
+        self.t0 = None
 
     def read(self, n=-1):
 
+        if self.t0 is None:
+            self.t0 = time.time()
+
         if self.cnt >= self.speed:
+            t1 = time.time()
+            dt = t1 - self.t0
+
+            if dt < Throttler.PERIOD:
+                time.sleep(Throttler.PERIOD - dt)
+
             self.cnt = 0
-            time.sleep(Throttler.PERIOD)
+            self.t0 = time.time()
+
         try:
             data = self.input.read(n)
         except IOError, ex:
